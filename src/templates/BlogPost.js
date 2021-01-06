282efdcb5bb7e10ea7a43737ad8d8f365e5c55e7
Jassem Gaaloul <jassemgaaloul123@gmail.com>
2021-01-06 05:14:29 +0100

    blogpost

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import { graphql,  } from "gatsby"
-
+import { graphql } from "gatsby"
 
 const data = graphql`
 query($slug: String!) {
@@ -14,11 +13,12 @@ query($slug: String!) {
   }
 }
 `
-const BlogPost = ({data}) => {
- 
+const BlogPost = ({ data }) => {
   return (
     <Layout>
       <h1> {data.mdx.frontmatter.title} </h1>
+      <p>Author : {data.mdx.frontmatter.author} </p>
+      <p> {data.mdx.body} </p>
     </Layout>
   )
 }
